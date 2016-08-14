2127f856e09742e672d8ea0ab6747524dc5ce9a6
rudhra rai <rudhra rai>
2016-08-14 22:43:10 +0530

    search function added

--- bst.cpp
+++ bst.cpp
@@ -7,7 +7,7 @@ struct bstnode
 	bstnode* left;
 	bstnode* right;
 };
-
+//create new node
 bstnode* getNewNode(int data)
 {
 	bstnode* newnode=new bstnode();
@@ -16,6 +16,8 @@ bstnode* getNewNode(int data)
 	newnode->right=NULL;
 	return newnode;
 }
+
+//insert new node
 bstnode* insert(bstnode* root,int data)
 {
 	if(root==NULL)
@@ -35,6 +37,16 @@ bstnode* insert(bstnode* root,int data)
 	return root;
 }
 
+bool search(bstnode* root,int data)
+{
+	if(root==NULL)return false;
+	else if(data == root->data)return true;
+	else if(data <= root->data)search(root->left,data);
+	else if(data > root->data)search(root->right,data);
+	else return false;
+	
+}
+//traverse in inorder
 void inorder(bstnode* root)
 {
 	if(root!=NULL)
@@ -52,5 +64,10 @@ int main()
  	insert(root,3);
  	insert(root,6);
  	inorder(root);
+ 	cout<<endl<<"enter the number to search:";
+ 	int num;
+ 	cin>>num;
+ 	if(search(root,num))cout<<"\ndata found";
+ 	else cout<<"\ndata not found";
 	return 0;
 }
